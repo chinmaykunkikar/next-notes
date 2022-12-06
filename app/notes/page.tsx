74d38645826039e7d3452321671e0773244cee0a
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-07 02:15:19 +0530

    page: use pocketbase APIs to fetch data

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link"
+import PocketBase from 'pocketbase'
+
+export const dynamic = "auto",
+    dynamicParams = true,
+    revalidate = 0,
+    fetchCache = 'auto',
+    runtime = 'nodejs',
+    prefferedRegion = 'auto'
 
 async function getNotes() {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=10',
-        { cache: "no-store" })
-    const data = await res.json()
+    const db = new PocketBase('http://127.0.0.1:8090/')
+    const data = await db.collection('notes').getList()
     return data?.items as any[]
 }
 
