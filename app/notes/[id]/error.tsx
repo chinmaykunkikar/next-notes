5a1c5d1b006b9bcdf420ed94574eca7d56ee8f5b
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-07 02:34:12 +0530

    notes: add loading and error states

---
+++ app/notes/[id]/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from "next/link";
+
+export default function Error() {
+    return (
+        <div>
+            <h3>There was an error.</h3>
+            <Link href="/notes">Go back to Notes</Link>
+        </div>
+    )
+
+}
